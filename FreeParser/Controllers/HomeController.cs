c8b6f21f24087d6a1dda9361df7d5985edb97f5b
Олександр Левченко <LevchenkoOVV@krok.edu.ua>
2021-04-08 18:48:47 +0300

    first commet

--- FreeParser/Controllers/HomeController.cs
+++ FreeParser/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace FreeParser.Controllers
 		[HttpGet]
 		public IActionResult Get()
 		{
-			return Ok("Get result is null");
+			return Ok("Get result is ");
 			/*TelegramBotClient client = await Bot.Get();
 			var info =  await client.GetWebhookInfoAsync();*/
 
